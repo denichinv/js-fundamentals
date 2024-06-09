784c4e6b05c35c0b65ab292d36ce50e7cc05e20f
denichinv <denichinv@gmail.com>
2024-06-09 15:20:19 +0100

    Add files via upload

---
+++ Fund_functions/addAndSubtract.js
@@ -0,0 +1,15 @@
+function subtract(num1, num2, num3) {
+  const add = function (a, b) {
+    return a + b;
+  };
+
+  const result = add(num1, num2) - num3;
+  console.log(result);
+}
+subtract(
+  1,
+
+  17,
+
+  30
+);
