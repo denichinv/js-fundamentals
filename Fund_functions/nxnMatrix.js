422bae7c3afdc782d6f593bff9324d5acd42970c
denichinv <denichinv@gmail.com>
2024-06-11 21:24:05 +0100

    Add files via upload

---
+++ Fund_functions/nxnMatrix.js
@@ -0,0 +1,13 @@
+function nxnMatrix(num) {
+    for (let row = 0; row < num; row++) {  
+      let result = "";
+      for (let add = 0; add < num; add++) {  
+        result += num + " ";
+      }
+      console.log(result.trim());  
+    }
+  }
+  
+  nxnMatrix(3);
+  
+
