1e32a169b8fd1c460d9787b388f99922d0e04e06
denichinv <denichinv@gmail.com>
2024-06-15 16:13:05 +0100

    Add files via upload

---
+++ arraysAdvanced/distinct-Array.js
@@ -0,0 +1,15 @@
+function distinctArray(arr) {
+  let newArr = [];
+
+  for (let num of arr) {
+    if (!newArr.includes(num)) {
+      newArr.push(num);
+    }
+  }
+  console.log(newArr.join(' '));
+}
+distinctArray([
+  20, 8, 12, 13, 4,
+
+  4, 8, 5,
+]);
