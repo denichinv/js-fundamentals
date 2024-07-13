116d12d92f543502b7ffc4aa9360c0e878f40832
denichinv <denichinv@gmail.com>
2024-07-13 14:22:25 +0100

    Add files via upload

---
+++ associative-arrays/phone-book.js
@@ -0,0 +1,20 @@
+function solve(arr) {
+  let phoneBook = {};
+
+  for (const row of arr) {
+    let [name, phoneNumber] = row.split(" ");
+
+    phoneBook[name] = phoneNumber;
+  }
+
+  for (const key in phoneBook) {
+    console.log(`${key} -> ${phoneBook[key]}`);
+  }
+}
+
+solve([
+  "Tim 0834212554",
+  "Peter 0877547887",
+  "Bill 0896543112",
+  "Tim 0876566344",
+]);
