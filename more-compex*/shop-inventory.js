e6a582a999f860f8cb8dff87a8402da7b4f64f66
denichinv <denichinv@gmail.com>
2024-08-24 21:15:38 +0100

    Add files via upload

---
+++ more-compex*/shop-inventory.js
@@ -0,0 +1,27 @@
+const inventory = {
+  sunglasses: 1900,
+  pants: 1088,
+  bags: 1344,
+};
+
+const myExecutor = (resolve, reject) => {
+  if (inventory.sunglasses > 0) {
+    resolve("Sunglasses order processed.");
+  } else {
+    reject("That item is sold out.");
+  }
+};
+
+function orderSunglasses() {
+  return new Promise(myExecutor);
+}
+
+const orderPromise = orderSunglasses();
+
+orderPromise
+  .then((message) => {
+    console.log(message);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
